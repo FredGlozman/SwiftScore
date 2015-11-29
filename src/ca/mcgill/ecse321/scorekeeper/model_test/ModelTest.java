89b3f56d4f1f579e5a88d01375bf79510e5c6e89
Steven D <steven.nfd@gmail.com>
2015-11-29 10:48:02 -0500

    Add JUnit test skeleton code

---
+++ src/ca/mcgill/ecse321/scorekeeper/model_test/ModelTest.java
@@ -0,0 +1,40 @@
+package ca.mcgill.ecse321.scorekeeper.model_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Set of test cases for the ca.mcgill.ecse321.scorekeeper.model package.
+ */
+public class ModelTest
+{
+	/**
+	 * JUnit setUp() method.
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	/**
+	 * JUnit tearDown() method.
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
