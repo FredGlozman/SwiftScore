1b5a6fd7127ec5cfc44bf9ae90bf79b8bb37ac4e
danielebercovici <daniele.bercovici@mail.mcgill.ca>
2015-12-02 19:18:13 -0500

    back end Create team

--- SwiftScore/CreateTeam.php
+++ SwiftScore/CreateTeam.php
@@ -2,9 +2,6 @@
 	<?php
 	$type = "CT";
 	$teamName = $_POST["teamName"];
-	$playerName = $_POST["playerName"];
-	$playerNumber = $_POST["playerNumber"];
-	$typePlayer = $_POST["type"];
 
-	exec("java -jar CreateTeam.jar Controller $type $teamName $playerName $playerNumber $typePlayer", $output);
+	exec("java -jar CreateTeam.jar Controller $type $teamName", $output);
 	?>
